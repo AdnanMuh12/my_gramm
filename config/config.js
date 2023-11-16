82554ed159b9a00eebe9c09053b907107afc6025
DewiLukas <dewilukas89@gmail.com>
2023-11-16 20:04:21 +0800

    main

--- config/config.js
+++ config/config.js
@@ -8,6 +8,13 @@ const config = {
         "host": process.env.DB_HOST_DEV,
         "dialect": process.env.DB_DIALECT_DEV
     },
+    // "test": {
+    //     "username": "root",
+    //     "password": null,
+    //     "database": "database_test",
+    //     "host": "127.0.0.1",
+    //     "dialect": "mysql"
+    // },
     "production": {
         "username": process.env.DB_USERNAME_PROD,
         "password": process.env.DB_PASSWORD_PROD,
