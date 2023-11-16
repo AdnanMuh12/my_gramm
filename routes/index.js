7ab6c017b825ec963dd2723f43a44cbb7a9665fd
DewiLukas <dewilukas89@gmail.com>
2023-11-16 18:19:07 +0800

    fixed routes

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-// const router = require("express").Router()
-
-// const userRoutes = require("./userRoutes")
-// const { authenticateToken } = require("../middleware/auth")
-
-// router.use("/users", authenticateToken, userRoutes)
-
-// module.exports = router
--- routes/photoRoutes.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router()
-
-const PhotoController = require("../controller/photoController")
-const { authenticateToken } = require("../middleware/auth")
-
-router.post("/photos", authenticateToken, PhotoController.createPhoto)
